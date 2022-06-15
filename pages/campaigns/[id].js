1f0eec5c4334ae385a982cce714f8dc57c1046dd
EricNguyen99 <vanthin7111999@gmail.com>
2022-06-15 23:44:37 +0700

    feat: added sidebar campaign detail

--- pages/campaigns/[id].js
+++ pages/campaigns/[id].js
@@ -1,95 +1,87 @@
-import {
-  Box,
-  Flex,
-  Avatar,
-  HStack,
-  Link,
-  IconButton,
-  Button,
-  Menu,
-  MenuButton,
-  MenuList,
-  MenuItem,
-  MenuDivider,
-  useDisclosure,
-  useColorModeValue,
-  Stack
-} from '@chakra-ui/react';
-import { HamburgerIcon, CloseIcon, AddIcon } from '@chakra-ui/icons';
+import { Box, Container, Flex, Heading, Stack, Text } from '@chakra-ui/layout';
+import { Table, TableCaption, Tbody, Td, Tfoot, Th, Thead, Tr } from '@chakra-ui/table';
+import { Tab, TabList, TabPanel, TabPanels, Tabs } from '@chakra-ui/tabs';
+import CampaignerCard from '../../components/CampaignerCard';
+import CampaignInfo from '../../components/CampaignInfo';
+import Layout from '../../components/Layout';
+import SupportersCard from '../../components/SupportersCard';
 
-const Links = ['Home'];
+export default function Campaign() {
+    return (
+        <Layout>
+            <Container maxW="container.xl" py={8}>
+                <Stack spacing={8}>
+                    <Heading textAlign="center">Lorem ipsum dolor sit amet</Heading>
 
-const NavLink = ({ children }) => (
-  <Link
-      px={2}
-      py={1}
-      rounded={'md'}
-      _hover={{
-          textDecoration: 'none',
-          bg: useColorModeValue('gray.200', 'gray.700')
-      }}
-      href={'/'}>
-      {children}
-  </Link>
-);
+                    <Flex flexDirection="row">
+                        <Box flex={7}>
+                            <Tabs isFitted variant="enclosed">
+                                <TabList mb="1em">
+                                    <Tab>About</Tab>
+                                    <Tab>Requests</Tab>
+                                </TabList>
+                                <TabPanels>
+                                    <TabPanel>
+                                        <Text>
+                                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia libero, aut
+                                            suscipit ex iusto quo harum perspiciatis laudantium alias maxime.
+                                        </Text>
+                                    </TabPanel>
+                                    <TabPanel>
+                                        <Table variant="striped" colorScheme="teal">
+                                            <TableCaption>Imperial to metric conversion factors</TableCaption>
+                                            <Thead>
+                                                <Tr>
+                                                    <Th>To convert</Th>
+                                                    <Th>into</Th>
+                                                    <Th isNumeric>multiply by</Th>
+                                                </Tr>
+                                            </Thead>
+                                            <Tbody>
+                                                <Tr>
+                                                    <Td>inches</Td>
+                                                    <Td>millimetres (mm)</Td>
+                                                    <Td isNumeric>25.4</Td>
+                                                </Tr>
+                                                <Tr>
+                                                    <Td>feet</Td>
+                                                    <Td>centimetres (cm)</Td>
+                                                    <Td isNumeric>30.48</Td>
+                                                </Tr>
+                                                <Tr>
+                                                    <Td>yards</Td>
+                                                    <Td>metres (m)</Td>
+                                                    <Td isNumeric>0.91444</Td>
+                                                </Tr>
+                                            </Tbody>
+                                            <Tfoot>
+                                                <Tr>
+                                                    <Th>To convert</Th>
+                                                    <Th>into</Th>
+                                                    <Th isNumeric>multiply by</Th>
+                                                </Tr>
+                                            </Tfoot>
+                                        </Table>
+                                    </TabPanel>
+                                </TabPanels>
+                            </Tabs>
+                        </Box>
+                        <Stack spacing={8} flex={3}>
+                            <CampaignInfo />
+                            <CampaignerCard />
+                            <SupportersCard />
+                        </Stack>
+                    </Flex>
+                </Stack>
+            </Container>
+        </Layout>
+    );
+}
 
-export default function Navbar() {
-  const { isOpen, onOpen, onClose } = useDisclosure();
-
-  return (
-      <>
-          <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
-              <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
-                  <IconButton
-                      size={'md'}
-                      icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
-                      aria-label={'Open Menu'}
-                      display={{ md: 'none' }}
-                      onClick={isOpen ? onClose : onOpen}
-                  />
-                  <HStack spacing={8} alignItems={'center'}>
-                      <Box>Logo</Box>
-                      <HStack as={'nav'} spacing={4} display={{ base: 'none', md: 'flex' }}>
-                          {Links.map((link) => (
-                              <NavLink key={link}>{link}</NavLink>
-                          ))}
-                      </HStack>
-                  </HStack>
-                  <Flex alignItems={'center'}>
-                      <Button variant={'solid'} colorScheme={'teal'} size={'sm'} mr={4} leftIcon={<AddIcon />}>
-                          New Campaign
-                      </Button>
-                      <Menu>
-                          <MenuButton as={Button} rounded={'full'} variant={'link'} cursor={'pointer'}>
-                              <Avatar
-                                  size={'sm'}
-                                  src={
-                                      'https://images.unsplash.com/photo-1493666438817-866a91353ca9?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9'
-                                  }
-                              />
-                          </MenuButton>
-                          <MenuList>
-                              <MenuItem>Link 1</MenuItem>
-                              <MenuItem>Link 2</MenuItem>
-                              <MenuDivider />
-                              <MenuItem>Link 3</MenuItem>
-                          </MenuList>
-                      </Menu>
-                  </Flex>
-              </Flex>
-
-              {isOpen ? (
-                  <Box pb={4} display={{ md: 'none' }}>
-                      <Stack as={'nav'} spacing={4}>
-                          {Links.map((link) => (
-                              <NavLink key={link}>{link}</NavLink>
-                          ))}
-                      </Stack>
-                  </Box>
-              ) : null}
-          </Box>
-
-          {/* <Box p={4}>Main Content Here</Box> */}
-      </>
-  );
+export async function getServerSideProps({ params }) {
+    return {
+        props: {
+            campaign: {}
+        }
+    };
 }
