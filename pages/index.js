4b8a9bacce3e9c09d92ac92b8d5c49605429711e
thinnguyen99 <thinnv@dgroup.co>
2022-06-15 07:54:16 +0700

    feat: add homepage

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,19 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import { useEffect } from 'react';
-import styles from '../styles/Home.module.css';
-import web3 from '../web3';
+import NavBar from '../components/NavBar';
+import Hero from '../components/Hero';
+import Footer from '../components/Footer';
+import Card from '../components/Card';
 
 export default function Home() {
     // useEffect(() => {
-    //     async function getData() {
-    //         const netId = await web3.eth.net.getId();
-    //         await window.ethereum.enable();
-    //         const accounts = await web3.eth.getAccounts();
-    //         // const balance = await web3.eth.getBalance(accounts[0]);
-
-    //         console.log(netId);
-    //         console.log(accounts);
-    //         // console.log(balance);
-    //     }
-
     //     getData();
     // }, []);
 
-    return <div className={styles.container}></div>;
+    return (
+        <div>
+            <NavBar />
+            <Hero />
+            <Card />
+            <Footer />
+        </div>
+    );
 }
