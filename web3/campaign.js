5d67f3ba114cf1b05b1975297635a7288b4905fa
EricNguyen99 <vanthin7111999@gmail.com>
2022-06-22 07:08:56 +0700

    add: smart contract to home

--- web3/campaign.js
+++ web3/campaign.js
@@ -1,6 +1,6 @@
-import web3 from './index';
-import Campaign from '../build/contracts/Campaign.json';
+// import web3 from './index';
+// import Campaign from '../build/contracts/Campaign.json';
 
-export default (address) => {
-  return new web3.eth.Contract(JSON.parse(Campaign.abi), address);
-};
+// export default (address) => {
+//     return new web3.eth.Contract(Campaign.abi, address);
+// };
