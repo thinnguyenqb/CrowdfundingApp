6971e3bf86cfcbf752840d337ccdedf579946c6f
thinnguyen99 <thinnv@dgroup.co>
2022-07-02 14:18:09 +0700

    restructure project

--- pages/index.js
+++ pages/index.js
@@ -2,33 +2,8 @@ import { useEffect } from 'react';
 import CardSlider from '../components/CardSlider';
 import Hero from '../components/Hero';
 import Layout from '../components/Layout';
-import web3 from '../web3';
-import campaignFactory from '../web3/campaignFactory';
-import campaign from '../web3/campaign';
 
 export default function Home() {
-    useEffect(() => {
-        async function getData() {
-            const netId = await web3.eth.net.getId();
-            await window.ethereum.enable();
-            const accounts = await web3.eth.getAccounts();
-            const balance = await web3.eth.getBalance(accounts[0]);
-
-            console.log(netId);
-            console.log(accounts);
-            console.log(balance);
-
-            // Address of contract
-            const campaignAddress = await campaign('0xcfb58aa8287fEe4Be63CE3672c28B24141253fe4')
-            console.log(campaignAddress);
-            // // const cp = await campaign(campaignAddress[0]).methods.getSummary().call();
-            // const cp = await campaignAddress.map(async (add) => await campaign(add).methods.getSumary().call());
-            // console.log(cp);
-        }
-
-        getData();
-    }, []);
-
     return (
         <Layout>
             <Hero />
